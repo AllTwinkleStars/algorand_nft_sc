30fcbbbf852d469984dc3a16779dd12b1522863f
AllTwinkleStars <KeepGrowinga@gmail.com>
2022-06-18 21:56:35 +0300

    modified

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-var express = require("express");
-var app = express();
-
-var port = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + "/public"));
-
-app.get("/", function(req, res) {
-    res.render("index");
+//server.js
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + "/build/favicon.ico"));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/ping", function(req, res) {
+    return res.send("pong");
 });
-
-app.listen(port, function() {
-    console.log("app running");
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+app.listen(port);
